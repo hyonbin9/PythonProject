07b671e1d1c7b933e75a8b0d84890ff91ffd84ee
hyonbin9 <hyonbin9@naver.com>
2021-09-15 09:08:12 +0900

    Initial File Upload

---
+++ Python file 1.py
@@ -0,0 +1,14 @@
+students = ["egoing","sori","maru"]
+grades = [2,1,4]
+print("students(1)",students[1])
+print("len(students)",len(students))
+print("min(students)",min(students))
+print("max(students)",max(students))
+print("sum(grades)",sum(grades))
+
+import statistics
+print("statistics.mean(grades)",statistics.mean(grades))
+
+import random
+print("random.choice(students)",random.choice(students))
+
